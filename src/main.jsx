80956f72fd1d3df2dcebc080df7f5046dc3e157e
kigundalevi <kigundalevi@gmail.com>
2025-03-18 17:26:32 +0300

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Cursor from "./components/ui/Cursor.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Cursor />
+    <Analytics />
     <App />
   </React.StrictMode>
 );
